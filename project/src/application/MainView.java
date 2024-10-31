ebe1ddce3887a422003ae8e714cb1ce4494b65c5
Roosevelt James <142815674+RooJames@users.noreply.github.com>
2024-10-31 18:16:33 -0400

    Update changes from main (buyer premium/commission)

--- project/src/application/MainView.java
+++ project/src/application/MainView.java
@@ -2,6 +2,7 @@ package application;
 
 import javafx.collections.ObservableList;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class MainView {
@@ -10,6 +11,12 @@ public class MainView {
     private ComboBox<Category> categoryComboBoxUserInterface;
     private TextField categoryInput;
     private Button addButton;
+    private TextField premiumInput;
+    private Button setPremiumButton;
+    private TextField commissionInput;
+    private Button setCommissionButton;
+    private Label buyerPremiumLabel;
+    private Label sellerCommissionLabel;
 
     public MainView(ObservableList<Category> categories) {
         tabPane = new TabPane();
@@ -20,10 +27,23 @@ public class MainView {
 
         categoryInput = new TextField();
         categoryInput.setPromptText("Enter category name");
+        categoryInput.setMaxWidth(2 * 400 / 3); // Set width to 2/3 of the screen length
 
         addButton = new Button("Add Category");
 
-        VBox systemAdminContent = new VBox(10, categoryComboBoxSystemAdmin, categoryInput, addButton);
+        premiumInput = new TextField();
+        premiumInput.setPromptText("Enter buyer's premium (%)");
+        premiumInput.setMaxWidth(2 * 400 / 3); // Set width to 2/3 of the screen length
+
+        setPremiumButton = new Button("Set Premium");
+
+        commissionInput = new TextField();
+        commissionInput.setPromptText("Enter seller's commission (%)");
+        commissionInput.setMaxWidth(2 * 400 / 3); // Set width to 2/3 of the screen length
+
+        setCommissionButton = new Button("Set Commission");
+
+        VBox systemAdminContent = new VBox(10, categoryComboBoxSystemAdmin, new HBox(10, categoryInput, addButton), new HBox(10, premiumInput, setPremiumButton), new HBox(10, commissionInput, setCommissionButton));
         Tab systemAdminTab = new Tab("System Admin", systemAdminContent);
         systemAdminTab.setClosable(false);
 
@@ -31,15 +51,18 @@ public class MainView {
         categoryComboBoxUserInterface = new ComboBox<>(categories);
         categoryComboBoxUserInterface.setPromptText("Category");
 
-        VBox userInterfaceContent = new VBox(10, categoryComboBoxUserInterface);
+        buyerPremiumLabel = new Label("Buyer's Premium: Not set");
+        sellerCommissionLabel = new Label("Seller's Commission: Not set");
+
+        VBox userInterfaceContent = new VBox(10, categoryComboBoxUserInterface, buyerPremiumLabel, sellerCommissionLabel);
         Tab userInterfaceTab = new Tab("User Interface", userInterfaceContent);
         userInterfaceTab.setClosable(false);
 
-        // Other Tab
-        Tab tab3 = new Tab("Tab 3", new Label("Content for Tab 3"));
-        tab3.setClosable(false);
+        // My Profile Tab
+        Tab myProfileTab = new Tab("My Profile", new Label("Content for My Profile"));
+        myProfileTab.setClosable(false);
 
-        tabPane.getTabs().addAll(systemAdminTab, userInterfaceTab, tab3);
+        tabPane.getTabs().addAll(systemAdminTab, userInterfaceTab, myProfileTab);
     }
 
     public TabPane getTabPane() {
@@ -60,5 +83,29 @@ public class MainView {
 
     public Button getAddButton() {
         return addButton;
+    }
+
+    public TextField getPremiumInput() {
+        return premiumInput;
+    }
+
+    public Button getSetPremiumButton() {
+        return setPremiumButton;
+    }
+
+    public TextField getCommissionInput() {
+        return commissionInput;
+    }
+
+    public Button getSetCommissionButton() {
+        return setCommissionButton;
+    }
+
+    public Label getBuyerPremiumLabel() {
+        return buyerPremiumLabel;
+    }
+
+    public Label getSellerCommissionLabel() {
+        return sellerCommissionLabel;
     }
 }
