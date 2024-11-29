77702afcac1e9840d663acf411cfa82ac821b6ee
Bazil Ashfaq <mbashfaq@valdosta.edu>
2024-11-28 20:31:12 -0500

    Update workspace.xml

---
+++ project/src/application/Main.java
@@ -0,0 +1,53 @@
+package application;
+
+//Will need to be revamped once our classes have been reorganized
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Tab;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private MainController controller;
+
+    @Override
+    public void start(Stage primaryStage) {
+        MainView view = new MainView(null);
+        controller = new MainController(view);
+
+        view.getListItemButton().setOnAction(event -> {
+            if (controller.getCategories().isEmpty()) {
+                view.getListItemErrorLabel().setText("Please add a category in the System Admin tab before listing an item.");
+                return;
+            }
+
+            ItemView itemView = new ItemView(controller.getCategories());
+            Tab createItemTab = new Tab("Create Item", itemView.getLayout());
+            createItemTab.setClosable(true);
+
+            new ItemController(itemView, controller.getCategories(), view.getTabPane(), createItemTab, controller.getItems(), controller);
+
+            view.getTabPane().getTabs().add(createItemTab);
+            view.getTabPane().getSelectionModel().select(createItemTab);
+        });
+
+        Scene scene = new Scene(view.getTabPane(), 800, 600);
+
+        primaryStage.setTitle("Auction System");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        if (controller != null) {
+            controller.shutdownScheduler();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
